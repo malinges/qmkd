6ba42862ed289b347bdcbbbd60a65d3dde33c79e
Simon Malinge <simon.malinge@gmail.com>
2021-05-12 20:50:23 -0400

    message types: use explicit values

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,9 @@
 export enum InputMessage {
-  RECORDING_UPDATE,
-  WPM_KEYPRESS,
+  RECORDING_UPDATE = 0x00,
+  WPM_KEYPRESS = 0x01,
 }
 
 export enum OutputMessage {
-  RECORDING_QUERY,
-  RECORDING_ACK,
+  RECORDING_QUERY = 0x00,
+  RECORDING_ACK = 0x01,
 }
